461350d73d793538b6e461fa5d5595ff76017154
Berzan Yildiz <berzanmikaili@gmail.com>
2020-06-18 17:26:17 +0200

    Added automatic log generation

--- test/server.go
+++ test/server.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
+	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+	w.WriteHeader(200)
 }
 
 func main() {
-
+	go writeLogs()
 	http.HandleFunc("/v1/input", hello)
-
 	http.ListenAndServe(":8090", nil)
+}
+
+func writeLogs() {
+	f, err := os.OpenFile("test/test.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	for {
+		if _, err := f.WriteString("Testing...\n"); err != nil {
+			panic(err)
+		}
+		time.Sleep(2 * time.Second)
+	}
 }

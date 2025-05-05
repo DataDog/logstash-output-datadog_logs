0f912f0de80abd58fcccb6152442bc31b4c29bf5
Brian Floersch <brian.floersch@datadoghq.com>
2025-05-05 16:05:39 -0400

    build test script fully working

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -6,15 +6,19 @@ gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113
 
 # Install RVM
 curl -sSL https://get.rvm.io | bash -s stable
+
+# Add RVM to PATH and source it
+echo 'source /etc/profile.d/rvm.sh' >> ~/.bashrc
+echo 'source /usr/local/rvm/scripts/rvm' >> ~/.bashrc
 source /etc/profile.d/rvm.sh
 source /usr/local/rvm/scripts/rvm
 
-# Install Java 11
+# Install Java 17
 apt-get update
-apt-get install -y openjdk-11-jdk
-update-alternatives --set java /usr/lib/jvm/java-11-openjdk-arm64/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-11-openjdk-arm64/bin/javac
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-arm64
+apt-get install -y openjdk-17-jdk
+update-alternatives --set java /usr/lib/jvm/java-17-openjdk-arm64/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-17-openjdk-arm64/bin/javac
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64
 
 # Setup directories
 WORKSPACE_DIR="/workspaces/logstash-output-datadog_logs"
@@ -34,6 +38,11 @@ rvm install $JRUBY_VERSION
 rvm use $JRUBY_VERSION --default
 gem install rake
 
+# Add RVM to PATH for current shell
+export PATH="$PATH:/usr/local/rvm/bin"
+export PATH="$PATH:/usr/local/rvm/rubies/$JRUBY_VERSION/bin"
+export PATH="$PATH:/usr/local/rvm/gems/$JRUBY_VERSION/bin"
+
 # Build Logstash
 export OSS=1
 export LOGSTASH_SOURCE=1
@@ -48,11 +57,9 @@ echo "JAVA_HOME: $JAVA_HOME"
 
 # Ignore Gradle exit code
 echo "Running Gradle..."
-./gradlew installDevelopmentGems || true
+./gradlew installDevelopmentGems
 
 echo "Running rake..."
-# Skip Java version check
-export SKIP_JAVA_VERSION_CHECK=true
 rake bootstrap
 
 # Verify Logstash
@@ -73,5 +80,3 @@ bundle install
 bundle exec rake vendor
 bundle exec rspec
 
-# Keep container running
-tail -f /dev/null 
